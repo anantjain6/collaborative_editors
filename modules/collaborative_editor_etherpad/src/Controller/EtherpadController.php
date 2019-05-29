2b9417704a69fb520e846402c9354c516eea6dcf
Anant Jain <anantjain60@gmail.com>
2019-05-29 22:59:52 +0530

    Directory & Basic structure

---
+++ modules/collaborative_editor_etherpad/src/Controller/EtherpadController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\collaborative_editor_etherpad\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Defines HelloController class.
+ */
+class HelloController extends ControllerBase {
+
+}
---
+++ src/Controller/CollaborativeEditorController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\collaborative_editor\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Defines HelloController class.
+ */
+class HelloController extends ControllerBase {
+
+}
