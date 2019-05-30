029e321b3771a737cfe0557290ba4c2cea2d2a45
Anant Jain <anantjain60@gmail.com>
2019-05-30 16:26:35 +0530

    Created menu and route of configuration

--- src/Controller/CollaborativeEditorsController.php
+++ src/Controller/CollaborativeEditorsController.php
@@ -9,4 +9,16 @@ use Drupal\Core\Controller\ControllerBase;
  */
 class CollaborativeEditorsController extends ControllerBase {
 
+  /**
+   * Display the markup.
+   *
+   * @return array
+   *   Return markup array.
+   */
+  public function plugins() {
+    return [
+      '#type' => 'markup',
+      '#markup' => $this->t('Configuration will go here'),
+    ];
+  }
 }
