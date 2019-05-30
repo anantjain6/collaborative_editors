57847665aaf279d0ba32f398c5c46ae6c011c295
Anant Jain <anantjain60@gmail.com>
2019-05-30 22:24:42 +0530

    Etherpad Configuration

---
+++ modules/collaborative_editor_etherpad/src/Form/EtherpadSettingsForm.php
@@ -0,0 +1,62 @@
+<?php  
+/**  
+ * @file  
+ * Contains Drupal\collaborative_editor_etherpad\Form\EtherpadSettingsForm.  
+ */  
+namespace Drupal\collaborative_editor_etherpad\Form;  
+use Drupal\Core\Form\ConfigFormBase;  
+use Drupal\Core\Form\FormStateInterface;  
+
+class EtherpadSettingsForm extends ConfigFormBase {  
+
+  /**  
+   * {@inheritdoc}  
+   */  
+  protected function getEditableConfigNames() {  
+    return [  
+      'collaborative_editor_etherpad.settings',  
+    ];  
+  }  
+
+  /**  
+   * {@inheritdoc}  
+   */  
+  public function getFormId() {  
+    return 'collaborative_editor_etherpad_settings';  
+  } 
+
+  /**  
+   * {@inheritdoc}  
+   */  
+  public function buildForm(array $form, FormStateInterface $form_state) {  
+    $config = $this->config('collaborative_editor_etherpad.settings');  
+
+    $form['etherpad_api_url'] = [  
+      '#type' => 'textfield',  
+      '#title' => $this->t('Etherpad API URL'),  
+      '#description' => $this->t('Enter Etherpad API URL. By default it is http://localhost:9001'),
+      '#default_value' => $config->get('etherpad_api_url'),  
+    ];  
+
+    $form['etherpad_api_key'] = [  
+      '#type' => 'textfield',  
+      '#title' => $this->t('Etherpad API Key'),  
+      '#description' => $this->t('Enter Etherpad API Key. You can find API Key in APIKEY.txtx on root directory of Etherpad.'),
+      '#default_value' => $config->get('etherpad_api_key'),  
+    ];  
+
+    return parent::buildForm($form, $form_state);  
+  } 
+
+  /**  
+   * {@inheritdoc}  
+   */  
+  public function submitForm(array &$form, FormStateInterface $form_state) {  
+    parent::submitForm($form, $form_state);  
+
+    $this->config('collaborative_editor_etherpad.settings')  
+      ->set('etherpad_api_url', $form_state->getValue('etherpad_api_url'))  
+      ->set('etherpad_api_key', $form_state->getValue('etherpad_api_key'))  
+      ->save();  
+  }  
+} 
---
+++ src/Form/CollaborativeEditorsSettingsForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\collaborative_editors\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Class CollaborativeEditorsSettingsForm.
+ */
+class CollaborativeEditorsSettingsForm extends ConfigFormBase {
+}
