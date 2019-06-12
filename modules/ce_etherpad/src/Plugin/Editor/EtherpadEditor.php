590123d7715efdc4fcab16215588521287bbfb10
Anant Jain <anantjain60@gmail.com>
2019-06-13 00:15:06 +0530

    Editor base code

---
+++ modules/ce_etherpad/src/Plugin/Editor/EtherpadEditor.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\ce_etherpad\Plugin\Editor;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\editor\Plugin\EditorBase;
+use Drupal\editor\Entity\Editor;
+
+/**
+ * Defines EtherpadEditor as an Editor plugin.
+ *
+ * @Editor(
+ *   id = "ce_etherpad",
+ *   label = "Etherpad",
+ *   supports_content_filtering = FALSE,
+ *   supports_inline_editing = TRUE,
+ *   is_xss_safe = FALSE,
+ *   supported_element_types = {
+ *     "textarea"
+ *   }
+ * )
+ */
+class EtherpadEditor extends EditorBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function getJSSettings (Editor $editor) {
+  	return [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getLibraries(Editor $editor) {
+  	return [];	
+  }
+}
