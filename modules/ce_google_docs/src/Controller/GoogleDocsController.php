5da5536e7add8700a6ea03f9885c34d8eb73f96d
Anant Jain <anantjain60@gmail.com>
2019-06-28 22:57:24 +0530

    test connection

--- modules/ce_google_docs/src/Controller/GoogleDocsController.php
+++ modules/ce_google_docs/src/Controller/GoogleDocsController.php
@@ -11,9 +11,38 @@ use Drupal\node\Entity\Node;
  * Class GoogleDocsController.
  */
 class GoogleDocsController extends ControllerBase {
-
-  public function addNode() {
-    var_dump(user_load_multiple()[1]->get('name')->value);
+  public function addNode(Request $request) {
+    $users = user_load_multiple();
+    $response = [];
+    for ($i = 0; $i <= count($users); $i++) {
+    	$response[$i]['id'] = $users[$i]->get('uid')->value;
+    	$response[$i]['name'] = $users[$i]->get('name')->value;
+    }
+    return new JsonResponse($response);
+  }
+  public function listUser(Request $request) {
+    $config = \Drupal::config('ce_google_docs.settings');
+  	if($request->query->get('apiKey') != $config->get('api_key'))
+  		return new JsonResponse(['error' => 'Invalid API Key.']);
+    $users = user_load_multiple();
+    $response = [];
+    for ($i = 1; $i < count($users); $i++) {
+    	$response[$i-1] = [
+    		'uid' => $users[$i]->get('uid')->value,
+    		'name' => $users[$i]->get('name')->value,
+    	];
+    }
+    return new JsonResponse($response);
+  }
+  public function testConnection(Request $request) {
+    $config = \Drupal::config('ce_google_docs.settings');
+    $response = [];
+  	if($request->query->get('apiKey') == $config->get('api_key')) {
+  		$response['msg'] = 'ok';
+  	} else  {
+  		$response['msg'] = 'error';
+  	}
+    return new JsonResponse($response);
   }
 
 }
